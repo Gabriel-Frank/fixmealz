aa7394743510326969182ce3c33fed4767d12478
Gabriel-Frank <68851149+Gabriel-Frank@users.noreply.github.com>
2020-08-20 22:08:13 +0100

    added files

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// function show(){
+//     document.getElementById('play').style.display = "none";
+//     document.getElementById('over').style.display = "block";
+//     document.getElementById('copy').style.display = "none";
+//     document.getElementById('paste').style.display = "none";
+// }
+// function play(){
+//     document.getElementById('play').style.display = "block";
+//     document.getElementById('over').style.display = "none";
+//     document.getElementById('copy').style.display = "none";
+//     document.getElementById('paste').style.display = "none";    
+// }
+// function init(){
+//     document.getElementById('play').style.display = "none";
+//     document.getElementById('over').style.display = "none";
+//     document.getElementById('copy').style.display = "block";
+//     document.getElementById('paste').style.display = "none";
+// }
+// function start(){
+//     document.getElementById('play').style.display = "none";
+//     document.getElementById('over').style.display = "none";
+//     document.getElementById('copy').style.display = "none";
+//     document.getElementById('paste').style.display = "block";
+// })
